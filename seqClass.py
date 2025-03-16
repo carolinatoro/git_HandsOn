78499cb3513df9eb19488f7a5434d607e50ff4b2
Carolina Toro <carolinatoro@Carolinas-MacBook-Air.local>
2025-03-16 17:33:14 -0500

    explained lines included in code

--- seqClass.py
+++ seqClass.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
 
+#import required modules for script to run
 import sys, re
 from argparse import ArgumentParser
 
+#argument parser helps manage command line arguments
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
+#\-\-seq handles the string input sequence 
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
+#motif to search within the sequence
 parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
 
+#handles an error if the script is run without arguments, stops and exits with status 1
 if len(sys.argv) == 1:
     parser.print_help()
     sys.exit(1)
 
+#user provided arguments are stored in args
 args = parser.parse_args()
 
+#converts the sequence to uppercase to handle lowercase not being recognised
 args.seq = args.seq.upper()                 # Note we just added this line
 
+#ensures that the sequence is valid rna or dna and then classifies it accoding to the presence of U or T
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
         print ('The sequence is DNA')
@@ -23,9 +31,9 @@ if re.search('^[ACGTU]+$', args.seq):
     else:
         print ('The sequence can be DNA or RNA')
 else:
-<<<<<<< HEAD
-    print ('The sequence is not DNA nor RNA, test change')
+    print ('The sequence is not DNA nor RNA, please check')
 
+#if motif search is provided (since it wasn't mandatory in the arguments at the beggining of the code. if it is, it convers to uppercase here too and searches the motif in the sequence
 if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
@@ -33,5 +41,3 @@ if args.motif:
         print("FOUND")
     else:
         print("NOT FOUND")
-
->>>>>>> motif
